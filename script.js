b619137547d516bddc2094f3529d1b3e563202dc
Rian-Luka <rianluka03@gmail.com>
2026-07-10 15:32:50 -0300

    Create script.js to handle user responses
    Added a script that prompts the user for answers and assigns values based on the input.

---
+++ script.js
@@ -0,0 +1,41 @@
+let um;
+let dois;
+let tres;
+let quatro;
+let cinco;
+
+let p1 = prompt("Para a pergunta ... Qual a resposta?").toUpperCase();
+let p2 = prompt("Para a pergunta ... Qual a resposta?").toUpperCase();
+let p3 = prompt("Para a pergunta ... Qual a resposta?").toUpperCase();
+let p4 = prompt("Para a pergunta ... Qual a resposta?").toUpperCase();
+let p5 = prompt("Para a pergunta ... Qual a resposta?").toUpperCase();
+
+if (p1 == 'A') {
+    um = 1;
+} else {
+    um = 0;
+}
+
+if (p2 == 'A') {
+    dois = 1;
+} else {
+    dois = 0;
+}
+
+if (p3 == 'A') {
+    tres = 1;
+} else {
+    tres = 0;
+}
+
+if (p4 == 'A') {
+    quatro = 1;
+} else {
+    quatro = 0;
+}
+
+if (p5 == 'A') {
+    quatro = 1;
+} else {
+    quatro = 0;
+}
